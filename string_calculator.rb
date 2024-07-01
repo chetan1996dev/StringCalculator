9a1b3d85635ea1c2b6f57041b6fa6f3f9091b5dd
chetan1996dev <chetan1996dev@gmail.com>
2024-07-01 18:23:57 +0530

    Final refactoring and cleanup the code

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,22 +3,32 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
 
-    delimiter = if input.start_with?("//")
-      input.match(%r{//(.+)\n})[1].strip # Extract custom delimiter specified after '//'
-    else
-      ",|\n" # Default delimiters like comma or newline
-    end
+    delimiter = extract_delimiter(input)
+    numbers = extract_numbers(input, delimiter)
+    validate_numbers(numbers)
 
-    result = input.gsub(%r{//(.+)\n}, "").split(/[#{delimiter}]/).map(&:to_i) # Remove delimiter and split string into numbers
-    validate_numbers(result)
-    result.sum
+    numbers.sum
   end
 
   private
 
-   # Method to validate numbers for negative values
+  # Method to extract delimiter from input string
+  def extract_delimiter(input)
+    if input.start_with?("//")
+      input.match(%r{//(.+)\n})[1].strip # Extract custom delimiter specified after '//'
+    else
+      ",|\n" # Default delimiters like comma or newline
+    end
+  end
+
+  # Method to extract numbers from string
+  def extract_numbers(input, delimiter)
+    input.gsub(%r{//(.+)\n}, "").split(/[#{delimiter}]/).map(&:to_i) # Remove delimiter and split string into numbers
+  end
+
+  # Method to validate numbers for negative values
   def validate_numbers(numbers)
     negatives = numbers.select { |num| num < 0 }
-    raise "negative numbers not allowed: #{ negatives.join(', ') }" if negatives.any?
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end
